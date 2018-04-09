8257d136aa13c33b63981683bda8aed466eb20b4
lesflibustiers <38003188+lesflibustiers@users.noreply.github.com>
2018-04-09 16:30:04 +0200

    Add files via upload

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,19 +1,8 @@
-=begin
-class temperature
-  attr_accessor :ftoc :eq
-  def initialize
-    @ftoc =  (5 / 9)eq
-    @eq = (9 / 5)ftoc
-  end
-end
-=end
-
-def     ftoc(tmp)
-               tmp = (tmp -32) * 5.0 / 9.0
+#write your code here
+def ftoc(temperature)
+  temperature = (temperature - 32) * 5.0/9.0
 end
 
-def     ctof(float_var)
-               float_var = (float_var * 9.0 / 5.0) + 32.0
+def ctof(temperature)
+  temperature = (temperature * 9.0/5.0) + 32
 end
-
-puts ftoc(4)
